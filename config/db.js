5fcd9bc635ac0f4b383d87a30142e26b9c30af8a
Anant Rungta <anantrungta1999@gmail.com>
2018-11-09 15:03:26 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,6 +2,6 @@ const mongoose=require('mongoose');
 
 mongoose.Promise =global.Promise;
 
-mongoose.connect('mongodb://anu:cool123@ds157383.mlab.com:57383/vote')
+mongoose.connect('Your db')
 .then(()=>console.log('MongoDb Connected'))
 .catch(err=>console.log(err));
